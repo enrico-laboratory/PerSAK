882e3ee4fabb46254eed4420c49930cc45798b10
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2021-12-01 11:37:01 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PerSAK',
+    version='0.1.0',
+    packages=[''],
+    url='https://github.com/enrico-laboratory/PerSAK',
+    license='MIT',
+    author='Enrico Ruggieri',
+    author_email='',
+    description='A collection of scripts which are executed via this CLI app'
+)
