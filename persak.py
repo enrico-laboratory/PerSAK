6837ace5d5d64004221b3067bd171385658194f4
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2021-12-02 12:16:57 +0100

    off command qemu implementation completed and tested

--- persak.py
+++ persak.py
@@ -1,5 +1,10 @@
-import click
+import os
 
+import click
+from scripts.qemu_utilities import QemuConnection
+from dotenv import load_dotenv
+
+load_dotenv()
 
 @click.group()
 # @click.option('--verbose', 'v', is_flag=True)
@@ -8,12 +13,55 @@ def cli():
 
 
 @cli.command()
-# @click.option('--string', default='World')
-# @click.argument('out', type=click.File('w'), default='-'
-#                   required=False)
-def off():
-    """Turn the pc off or reboot it"""
-    pass
+@click.option('--list', '-l', 'list_',
+              is_flag=True,
+              help="List all the active domains")
+@click.option('--down-selected', '-s',
+              type=str,
+              multiple=True,
+              help='Shutdown selected domain')
+@click.option('--reb', '-r',
+              is_flag=True,
+              help="Reboot the VM host")
+@click.option('--down', '-d',
+              is_flag=True,
+              help="Shutdown the VM host")
+def off(list_, down_selected, reb, down):
+    """Turn the VM host off or reboot it,
+    also list the active VMs and can shutting down selected one"""
+
+    click.echo("Establishing connection...")
+    conn = QemuConnection()
+
+    if list_:
+        print('Active domains:')
+        for domain in conn.list_active_domain():
+            print(domain)
+        click.echo("...closing connection.")
+        conn.close_connection()
+
+    if down_selected:
+        for domain in down_selected:
+            conn.shutdown_domain(domain)
+            print("Shutting down \"" + domain + "\" domain...")
+        click.echo("...closing connection.")
+        conn.close_connection()
+
+    if down or reb:
+        click.echo("Shutting down all VMs except OPNSense...")
+        active_domains = conn.list_active_domain()
+        index = active_domains.index(os.environ.get("OPNSENSE_DOMAIN"))
+        # removing OPNSense domain from list
+        active_domains.pop(index)
+        for domain in active_domains:
+            print("Shutting down \"" + str(domain) + "\" domain...")
+            conn.shutdown_domain(domain)
+        if down:
+            # TODO System shutdown
+            click.echo("Shutdown!!")
+        if reb:
+            # TODO System reboot
+            click.echo("Rebooting!!")
 
 
 @cli.command()
