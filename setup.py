a9cc9378d60eb2cf29a86ca2a4142c453938dc0f
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2021-12-08 20:44:19 +0100

    updated setup.py with install requires

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name='PerSAK',
     version='0.1.0',
-    packages=[''],
+    packages=[],
     url='https://github.com/enrico-laboratory/PerSAK',
     license='MIT',
     author='Enrico Ruggieri',
     author_email='',
     description='A collection of scripts which are executed via this CLI app',
     install_requires=[
-        'Click',
+        'click','libvirt-python', 'python-dotenv'
     ],
     entry_points='''
         [console_scripts]
